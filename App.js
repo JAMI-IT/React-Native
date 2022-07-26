651b92b4f57645464da698a70b5ee59f2672d088
jami <abdulrehmanjami73@gmail.com>
2022-07-26 14:38:11 +0500

    RN

--- App.js
+++ App.js
@@ -1,20 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-console.error("hrkllo");
-export default function App() {
+import React from 'react';
+import { View, Text, SafeAreaView, StyleSheet } from 'react-native';
+import ColorBox from './component/ColorBox';
+const app = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+      <View style={styles.contaner}>
+        <Text style={styles.text}>Here are some boxes Of deffrent Colors</Text>
+        <ColorBox colorName="Cyan" hexCode="#2aa198" />
+        <ColorBox colorName="Blue" hexCode="#268bd2" />
+        <ColorBox colorName="Magenta" hexCode="#d33682" />
+        <ColorBox colorName="Orange" hexCode="#cb4b16" />
+        <ColorBox colorName="Cyan" hexCode="#2aa198" />
+      </View>
+    </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  contaner: {
+    marginVertical:20,
+    paddingHorizontal: 10,
+    paddingTop: 40,
   },
+  text: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+ 
 });
+
+export default app;
