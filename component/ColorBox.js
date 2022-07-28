619622dbb01bcf7eb6c561e2ae4db0ef930688f9
jami <abdulrehmanjami73@gmail.com>
2022-07-28 11:28:18 +0500

    RN

--- component/ColorBox.js
+++ component/ColorBox.js
@@ -2,25 +2,34 @@ import react from "react";
 import { View, Text,StyleSheet } from 'react-native';
  
 const ColorBox = (props) => {
-    const boxColor= {
-        backgroundColor: props.hexCode,   
-    }
+ 
+  const boxColor = {
+    backgroundColor: props.hexCode,
+  };
+   const textcolor = {
+     color:
+       parseInt(props.hexCode.replace('#', ''), 16) > 0xffffff / 1.1
+         ? 'black'
+         : 'white',
+   };
     return (
         <View style={[styles.box,boxColor]}>
-            <Text style={styles.boxtext}>{props.colorName}:{props.hexCode} </Text>
+            <Text style={[styles.boxtext,textcolor]}>{props.colorName}:{props.hexCode} </Text>
         </View>
     );
 };
+
+
 
 const styles = StyleSheet.create({
   box: {
     justifyContent: 'center',
     alignItems: 'center',
-    padding: 10,
+    backgroundColor:'teal',
     marginBottom: 20,
+    padding:20,
     borderRadius: 20,
   },
   boxtext: { color: 'white', fontWeight: 'bold' },
 });
-
 export default ColorBox;
